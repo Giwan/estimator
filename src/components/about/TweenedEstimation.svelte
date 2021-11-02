c7cc1ace375770c16379d6126c1e2f19f262e7d9
Giwan Persaud <giwan.persaud@gmail.com>
2021-11-02 20:52:07 +0100

    tweened estimation

---
+++ src/components/about/TweenedEstimation.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+
+	const fibRange = [0, 1, 2, 3, 5, 8, 13, 20];
+	const estimation = tweened(0, {
+		duration: 250,
+        easing: cubicOut
+	});
+
+	function handleClick(e) {
+		$estimation = fibRange[e.currentTarget.value];
+	}
+</script>
+
+<h1>
+	{Number.parseFloat($estimation).toPrecision(3)}
+	<!-- {Math.round($estimation)} -->
+</h1>
+<div class="container">
+<input type="range" min="0" max="7" on:input={handleClick} />
+</div>
+
+<style>
+
+    h1 {
+        width: 100%;
+        text-align: center;
+        font-size: 3em;
+    }
+    .container {
+        width: 80%;
+        margin: var(--unit) auto;
+        /* background-color: red; */
+    }
+    input[type="range"] {
+        width: 100%;
+        
+    }
+</style>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,7 @@
     import FlipDemo from "../components/about/FlipDemo.svelte";
     import Counter from "../components/about/Counter.svelte";
     import SliderDemo from "../components/about/SliderDemo.svelte";
+import TweenedEstimation from "../components/about/TweenedEstimation.svelte";
     // import ExplanationSample from "../components/ExplanationSample.svelte";
 </script>
 
@@ -9,4 +10,5 @@
 <!-- <Counter /> -->
 <!-- <ExplanationSample /> -->
 <!-- <FlipDemo /> -->
-<SliderDemo />
+<!-- <SliderDemo /> -->
+<TweenedEstimation />
