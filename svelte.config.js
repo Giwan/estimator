87aa8109651d8ee04ef3c3d25bffc5855ee463a0
Giwan Persaud <giwan.persaud@gmail.com>
2022-02-17 19:11:57 +0100

    suggestions from netlify docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
-/** @type {import('@sveltejs/kit').Config} */
-import netlify from '@sveltejs/adapter-netlify';
+
+import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-auto';
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: netlify(),
+		adapter: adapter(),
 	}
 };
 
